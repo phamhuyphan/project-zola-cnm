b9614b22d8319f725559b4dc025a62d119237eb3
phamhuyphan <huyphanpham29092001@gmail.com>
2022-10-08 12:24:11 +0700

    backend api comment update replies

--- backend/routes/commentRoutes.js
+++ backend/routes/commentRoutes.js
@@ -2,7 +2,10 @@ const express = require("express");
 const {
     getAllComments,
     createComment,
-    deleteComment
+    deleteComment,
+    updateComment,
+    feedbackComment,
+    feedbackNewComment
 } = require("../controllers/commentControllers");
 const { sendMessage } = require("../controllers/messageControllers");
 const { protect } = require("../middleware/authMiddleware");
@@ -13,5 +16,8 @@ const router = express.Router();
 router.route("/:postId").get(protect, getAllComments);
 router.route("/").post(protect, createComment);
 router.route("/").delete(protect, deleteComment);
+router.route("/post/:postId/comments/:commentId/updatecmt").put(protect,updateComment)
+router.route("/post/:postId/comments/:commentId/replies/new").post(protect,feedbackNewComment);
+router.route("/post/:postId/comments/:commentId/replies").post(protect,feedbackComment);
 
 module.exports = router;
